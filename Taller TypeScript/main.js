2445d52507fa312feb7fb48c41e5a7de1514a038
Juan Jose Diaz <123647043+jjdiazo1@users.noreply.github.com>
2024-04-13 11:41:18 -0500

    Delete Taller TypeScript directory

--- Taller TypeScript/main.js
+++
@@ -1,8 +0,0 @@
-import { series } from './data.js';
-var tableBody = document.getElementById('series-table-body');
-if (tableBody) {
-    series.forEach(function (serie, index) {
-        var row = "<tr>\n                            <th scope=\"row\">".concat(index + 1, "</th>\n                            <td>").concat(serie.name, "</td>\n                            <td>").concat(serie.channel, "</td>\n                            <td>").concat(serie.seasons, "</td>\n                        </tr>");
-        tableBody.insertAdjacentHTML('beforeend', row);
-    });
-}
